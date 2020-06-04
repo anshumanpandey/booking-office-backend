9a5e5ce4d715e70f5f310c545af3761b93d777cf
leo <leo_9_t@hotmail.com>
2020-06-04 16:40:52 -0400

    ADDED creation of user

--- routes/super/client.super.route.js
+++ routes/super/client.super.route.js
@@ -1,17 +1,44 @@
 const express = require('express');
+const { Op } = require("sequelize");
 const router = express.Router();
 const AsyncMiddleware = require('../../utils/AsyncMiddleware');
 const UserModel = require('../../model/UserModel');
 const guard = require('express-jwt-permissions')({ permissionsProperty: 'type' });
 
 router.get('/super/client', guard.check('super_admin'), AsyncMiddleware(async (req, res) => {
-  const clients = await UserModel.findAll({ where: { type: 'company' }, include: { all: true } });
+  const clients = await UserModel.findAll({ where: { [Op.or]: [{ type: 'supplier' }, { type: 'broker' }] }, include: { all: true } });
 
   res.send(clients.map(client => {
     const c = client.toJSON();
     delete c.password
     return c;
   }));
+}));
+
+router.post('/super/client', guard.check('super_admin'), AsyncMiddleware(async (req, res) => {
+  const {
+    email,
+    password,
+    confirmPassword,
+    costPerClick,
+    type
+  } = req.body;
+
+  if (!email) throw new Error("Missing email fields");
+  if (!password) throw new Error("Missing password fields");
+  if (!confirmPassword) throw new Error("Missing confirmPassword fields");
+  if (!type) throw new Error("Missing type fields");
+  if (!costPerClick) throw new Error("Missing costPerClick fields");
+
+  const client = await UserModel.findOne({ where: { email }});
+
+  if (client) throw new Error("Email already register");
+  if (password !== confirmPassword) throw new Error("Password not match");
+  if (!(new RegExp('(supplier|broker)')).test(type)) throw new Error("invalid type");
+
+  await UserModel.create({ email, password: await UserModel.generateHash(password), type, costPerClick});
+
+  res.send({ sucess: "Supplier created"});
 }));
 
 router.put('/super/edit', guard.check('super_admin'), AsyncMiddleware(async (req, res) => {
