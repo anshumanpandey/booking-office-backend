28a28d5eb1148b72b7bdb2dd791042e5017073d0
leo <leo_9_t@hotmail.com>
2020-06-06 11:30:50 -0400

    FIX on response

--- routes/my-clicks.route.js
+++ routes/my-clicks.route.js
@@ -2,12 +2,27 @@ const express = require('express');
 const router = express.Router();
 const AsyncMiddleware = require('../utils/AsyncMiddleware');
 const ClickTrack = require('../model/ClickTrack');
+const UserModel = require('../model/UserModel');
 
 router.get('/clicks/', AsyncMiddleware(async (req, res) => {
 
-    const clicks = await ClickTrack.findAll({ where: {UserId: req.user.id}, order: [['created_at', 'DESC']] })
+    const clicks = await ClickTrack.findAll({
+        where: {UserId: req.user.id},
+        include: [{ model: UserModel }],
+        order: [['created_at', 'DESC']]
+    })
 
-    res.send(clicks);
+    const data = clicks.map(i => {
+        const json = i.toJSON();
+        return {
+            ...json,
+            User: {
+                id: json.User.id,
+                companyName: json.User.companyName,
+            }
+        }
+    })
+    res.send(data);
 }))
 
 module.exports = router;
