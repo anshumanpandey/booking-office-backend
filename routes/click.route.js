7e8b25bafe407fc9982f6ac2e1af25082fcc014d
leo <leo_9_t@hotmail.com>
2020-06-06 10:41:41 -0400

    ADDED time checking for clicks

--- routes/click.route.js
+++ routes/click.route.js
@@ -6,9 +6,10 @@ const UserModel = require('../model/UserModel');
 const ClickTrackModel = require('../model/ClickTrack');
 const sequelize = require('../utils/Database');
 const Decimal = require('decimal.js');
+const moment = require('moment');
 
 router.post('/public/click', AsyncMiddleware(async (req, res) => {
-  const { 
+  const {
     grcgds_supplier_name,
     country_code,
     ip,
@@ -18,21 +19,41 @@ router.post('/public/click', AsyncMiddleware(async (req, res) => {
   } = req.body;
 
   await sequelize.transaction(async (t) => {
-    const user = await UserModel.findOne({ where: { companyName: grcgds_supplier_name }, transaction: t });
-
+    const user = await UserModel.findOne({ where: { companyName: grcgds_supplier_name }, ransaction: t });
     if (!user) throw new Error("Supplier not found");
+
+    const startOfDay = moment().startOf('day');
+    const endOfDay = moment().endOf('day');
+
+    const click = await ClickTrackModel.findOne({
+      where: {
+        ip,
+        UserId: user.id,
+      },
+      order: [['created_at', 'DESC']],
+      transaction: t,
+    });
+
+    if (click) {
+      const clickDate = moment(click.created_at);
+
+      if (clickDate.isBetween(startOfDay, endOfDay)) {
+        // we alredy tracked a click from this IP for this user... skip
+        return;
+      }
+    }
 
     if (user.credits > 0) {
       const data = {
         ip,
         country: country_code,
-        supplierName: orignal_supplier_name, 
+        supplierName: orignal_supplier_name,
         UserId: user.id,
         pickupLocation,
         dropoffLocation,
       }
       await ClickTrackModel.create(data, { transaction: t });
-      await user.update({ credits: new Decimal(user.credits).minus(user.costPerClick), balance: new Decimal(user.balance).minus(user.costPerClick) }, { transaction: t });
+      await user.update({credits: new Decimal(user.credits).minus(user.costPerClick), balance: new Decimal(user.balance).minus(user.costPerClick) }, { transaction: t });
     } else {
       throw new Error("Service not available");
     }
@@ -44,7 +65,7 @@ router.post('/public/click', AsyncMiddleware(async (req, res) => {
 
 router.get('/public/unavailables', AsyncMiddleware(async (req, res) => {
 
-  const users = await UserModel.findAll({ where: { type: { [Op.not]: 'super_admin'} } });
+  const users = await UserModel.findAll({ where: { type: { [Op.not]: 'super_admin' } } });
 
   res.send(users.filter(u => u.credits <= 0 && u.companyName).map(i => ({ companyName: i.companyName })));
 }));
