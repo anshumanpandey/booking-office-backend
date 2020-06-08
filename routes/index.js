4478d443a4e11bc825e1f0a0747f7a5bdce3dd34
leo <leo_9_t@hotmail.com>
2020-06-08 10:05:56 -0400

    ADDED new routes

--- routes/index.js
+++ routes/index.js
@@ -6,8 +6,10 @@ const payments = require('./payments.route');
 const ClientRoute = require('./client.route');
 const ClickRoute = require('./click.route');
 const superClient = require('./super/client.super.route');
-const blacklisted = require('./super/blacklistedCompanies.route');
+const blacklistedSuper = require('./super/blacklistedCompanies.route');
+const blacklisted = require('./blacklistedCompanies.route');
 
+router.use(blacklistedSuper);
 router.use(blacklisted);
 router.use(ClickRoute);
 router.use(MyClicks);
